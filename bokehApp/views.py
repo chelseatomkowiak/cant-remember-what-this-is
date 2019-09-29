4dfd7db1d523722b3fcd3518cf0b1165435d7e8f
Chelsea Tomkowiak <tika.xo@gmail.com>
2019-09-28 23:51:50 -0500

    dont know what this is

---
+++ bokehApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import HoverTool
+
+def starter(request):
+    plot = figure()
+    plot.circle([1, 10, 35, 27], [0, 0, 0, 0], size=20, color="blue")
+    script, div = components(plot)
+    return render(request, 'starter.html', {'script':script, 'div':div})
